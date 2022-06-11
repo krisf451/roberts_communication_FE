9054ecd6a3612eb9660fff676b1a19ded4d05a84
Kristian Fulkerson <kristianf451@gmail.com>
2022-06-10 17:43:11 -0700

    init :alien:

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      backgroundColor: {
+        "main-bg": "#FAFBFB",
+        "main-dark-bg": "#20232A",
+        "secondary-dark-bg": "#33373E",
+        "light-gray": "#F7F7F7",
+        "half-transparent": "rgba(0, 0, 0, 0.5)",
+      },
+      backgroundImage: {
+        "hero-pattern":
+          "url('https://images.unsplash.com/photo-1500382017468-9049fed747ef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80')",
+      },
+    },
+  },
+  plugins: [require("tailwind-scrollbar-hide")],
+};
