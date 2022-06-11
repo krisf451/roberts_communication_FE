c63be3e9affa76f538661f53471cea38b0a92767
Kristian Fulkerson <kristianf451@gmail.com>
2022-06-11 01:45:17 -0700

    feat: filter working and tested by first name, last name, and full name first+last

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from "react";
+import { Loader } from "../components";
 import { useSelector, useDispatch } from "react-redux";
-import { getRandomUsers, pageUp, pageDown } from "../redux/features/usersSlice";
+import {
+  getRandomUsers,
+  pageUp,
+  pageDown,
+  reset,
+} from "../redux/features/usersSlice";
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 import { useNavigate } from "react-router";
 
 const Dashboard = () => {
-  const { users, isLoading, isError, isSuccess, message, page } = useSelector(
-    (state) => state.users
-  );
+  const { users, page } = useSelector((state) => state.users);
   const [searchTerm, setSearchTerm] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -15,14 +19,16 @@ const Dashboard = () => {
   useEffect(() => {
     dispatch(getRandomUsers(page));
     navigate(`/dashboard?page=${page}`);
+
+    return () => dispatch(reset());
   }, [dispatch, page, navigate]);
 
   return (
     <div className="min-h-screen dark:bg-main-dark-bg dark:text-white">
+      <h1 className="text-2xl font-extrabold text-center pt-4">
+        Search by Name
+      </h1>
       <div className="p-4 flex justify-center items-center w-96 mx-auto">
-        <label htmlFor="search" className="mr-2 block">
-          Search
-        </label>
         <input
           type="text"
           id="search"
@@ -31,8 +37,9 @@ const Dashboard = () => {
           onChange={(e) => setSearchTerm(e.target.value)}
         />
       </div>
+      {/* User Cards */}
       <div className="w-full flex flex-col justify-center items-center">
-        <div className="border-b p-4">
+        <div className="border-b p-4 w-full sm:w-96 text-center transition-all duration-300 ease-linear">
           {users
             ?.filter((user) => {
               return (
@@ -41,20 +48,38 @@ const Dashboard = () => {
                   .indexOf(searchTerm.toLowerCase()) !== -1 ||
                 user?.name?.last
                   .toLowerCase()
+                  .indexOf(searchTerm.toLowerCase()) !== -1 ||
+                `${user?.name?.first} ${user?.name?.last}`
+                  .toLowerCase()
                   .indexOf(searchTerm.toLowerCase()) !== -1
               );
             })
             ?.map((user, i) => (
-              <div key={i}>
+              <div
+                key={i}
+                className={`${
+                  i % 2 === 0 || i === 0
+                    ? "bg-gray-100 dark:bg-secondary-dark-bg"
+                    : ""
+                } transition-all duration-300 ease-in-out hover:bg-gray-400 cursor-pointer dark:bg-main-dark-bg dark:hover:bg-gray-500 p-2 rounded-md`}
+              >
                 {user?.name?.first} {user?.name?.last}
               </div>
             ))}
         </div>
 
-        <div className="flex w-32 justify-between items-center mt-4">
-          <AiOutlineArrowLeft onClick={() => dispatch(pageDown())} />
+        <div className="flex w-48 justify-evenly items-center mt-4">
+          <AiOutlineArrowLeft
+            size={45}
+            onClick={() => dispatch(pageDown())}
+            className="cursor-pointer"
+          />
           <p>{page}</p>
-          <AiOutlineArrowRight onClick={() => dispatch(pageUp())} />
+          <AiOutlineArrowRight
+            size={45}
+            onClick={() => dispatch(pageUp())}
+            className="cursor-pointer"
+          />
         </div>
       </div>
     </div>
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Navbar } from "./Navbar";
 export { default as Home } from "./Home";
 export { default as Dashboard } from "./Dashboard";
 export { default as UserDetails } from "./UserDetails";
+export { default as Loader } from "./Loader";
