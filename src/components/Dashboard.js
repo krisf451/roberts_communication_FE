4188eeb68f691eff8cf024340d7e3fce5dfaa71a
Kristian Fulkerson <kristianf451@gmail.com>
2022-06-10 20:28:42 -0700

    feat: navbar done

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Dashboard = () => {
-  return <div className="">Dashboard</div>;
+  return (
+    <div className="h-screen dark:bg-main-dark-bg dark:text-white">
+      Dashboard
+    </div>
+  );
 };
 
 export default Dashboard;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Home = () => {
-  return <div className="">Home</div>;
+  return (
+    <div className="h-screen dark:bg-main-dark-bg dark:text-white">Home</div>
+  );
 };
 
 export default Home;
